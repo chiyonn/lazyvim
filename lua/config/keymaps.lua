782860fae69b63ca3839046f1a80362aae3431d8
chiyonn <kud0h.chi4@gmail.com>
2023-11-16 15:45:38 +0900

    cloned lazyvim and set colorscheme and some keymaps

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("i", "jk", "<ESC>l", opts)
+
+keymap.set("n", "<leader>w", ":w<CR>", opts)
+keymap.set("n", "<leader>q", ":q<CR>", opts)
